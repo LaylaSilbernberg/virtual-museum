f764a00bf010c871954b140973ecc6b81af85bb1
sabin <sabin.bajracharya@appliedtechnology.se>
2023-03-27 10:47:16 +0200

    DAO with constructor and getter setter ...

--- src/main/java/tetrad/virtualmuseum/DAO/Gallery.java
+++ src/main/java/tetrad/virtualmuseum/DAO/Gallery.java
@@ -11,4 +11,46 @@ public class Gallery {
     private String galleryName;
     private String description;
     private String imageId;
+
+    public Gallery() {
+    }
+
+    public Gallery(int id, String galleryName, String description, String imageId) {
+        this.id = id;
+        this.galleryName = galleryName;
+        this.description = description;
+        this.imageId = imageId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGalleryName() {
+        return galleryName;
+    }
+
+    public void setGalleryName(String galleryName) {
+        this.galleryName = galleryName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(String imageId) {
+        this.imageId = imageId;
+    }
 }
