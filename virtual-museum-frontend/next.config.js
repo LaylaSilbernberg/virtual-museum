5c14fb50ec55205876615340fbb7a12706223b2e
Layla <layla.silbernberg@appliedtechnology.se>
2023-03-27 16:14:11 +0200

    images load in

--- virtual-museum-frontend/next.config.js
+++ virtual-museum-frontend/next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.artic.edu',
+        port: '',
+        pathname: '/iiif/2/**',
+      },
+    ],
+  },
   experimental: {
     appDir: true,
   },
---
+++ virtual-museum-frontend/src/app/(Props)/props.ts
@@ -0,0 +1,6 @@
+export interface ArtworkProps {
+    title:            string;
+    artist_display:   string;
+    place_of_origin:  string;
+    image_id:         string;
+}
