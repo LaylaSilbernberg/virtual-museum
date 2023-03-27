5bfa45c50102ef9c39f67e5dca70e90803eb32c8
Layla <layla.silbernberg@appliedtechnology.se>
2023-03-27 15:38:40 +0200

    Fetch works but useEffect fires twice

--- virtual-museum-frontend/src/app/page.tsx
+++ virtual-museum-frontend/src/app/page.tsx
@@ -2,8 +2,6 @@
 
 import Image from 'next/image'
 import MainGallery from './(Components)/MainGallery';
-import styles from './page.module.css'
-
 
 export default function Home() {
   return <MainGallery/>;
