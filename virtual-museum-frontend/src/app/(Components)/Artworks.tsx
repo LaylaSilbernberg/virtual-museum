71f0dee39e4c56b5ccd1d137f2cc1f0c5f925784
Layla <layla.silbernberg@appliedtechnology.se>
2023-03-28 12:45:29 +0200

    Image expand but does not centre

--- virtual-museum-frontend/src/app/(Components)/Artworks.tsx
+++ virtual-museum-frontend/src/app/(Components)/Artworks.tsx
@@ -1,15 +1,46 @@
-import React from 'react'
+import React, { ForwardRefExoticComponent, Ref, useRef, useState } from 'react'
 import { ArtworkProps } from '../(Props)/props'
 import Image from "next/image";
 
 const Artworks = ({image_id, title, artist_display, place_of_origin, thumbnail}: ArtworkProps) => {
+
+    const [openImage, setOpenImage] = useState<Boolean>(false);
+    const imageRef = useRef<any>()
+
+    openImage ? document.body.style.overflow = "hidden" : document.body.style.overflow = "auto";    
+
   return (
-    <Image 
-    src={`https://www.artic.edu/iiif/2/${image_id}/full/843,/0/default.jpg`} 
-    alt={`${thumbnail.alt_text}`}
-    width={843}
-    height={500}
-    priority/>
+
+    <>
+    
+    {
+    (openImage &&  
+      <div className="largeImage__container">
+      <Image
+                  className='largeImage__artwork'
+                  src={`https://www.artic.edu/iiif/2/${image_id}/full/843,/0/default.jpg`}
+                  alt={`${thumbnail.alt_text}`}
+                  width={600}
+                  height={600}
+                  onClick={() => setOpenImage(false)}
+                  priority 
+                  ref={imageRef}
+                  />
+      </div>
+     )
+     } 
+
+      <div className="image__card">
+              <Image
+                  className='image__artwork'
+                  src={`https://www.artic.edu/iiif/2/${image_id}/full/400,/0/default.jpg`}
+                  alt={`${thumbnail.alt_text}`}
+                  width={300}
+                  height={300}
+                  onClick={() => {
+                    setOpenImage(true)}}
+                  priority />
+        </div></>
   )
 }
 
