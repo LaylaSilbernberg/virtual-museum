27b686f95a458559c618d0f8b75431c7b3772442
sabin <sabin.bajracharya@appliedtechnology.se>
2023-03-28 10:02:32 +0200

    Added basics PersonalGallery and About page ...

---
+++ virtual-museum-frontend/src/app/about/page.tsx
@@ -0,0 +1,17 @@
+
+const About = () => {
+
+    return (
+        <div>
+            <h1> Welcome to the VIRTUAL MUSEUM </h1>
+            <h2> Mission statement </h2>
+            <h2> History </h2>
+            <h2> Collections </h2>
+            <h2> Contact Informations </h2>
+            <p> Something to put in here the details </p>
+            <p> We must decide what we are going to add to the about page ... </p>
+        </div>
+    )
+}
+
+export default  About
---
+++ virtual-museum-frontend/src/app/personal_gallery/page.tsx
@@ -0,0 +1,15 @@
+
+const PersonalGallery = () => {
+
+    return (
+        <div>
+            <h1> This is the Personal Gallery </h1>
+            <h2> I like this pictures </h2>
+            <h2> This is my favorite pictures </h2>
+            <p> Something to put in here the details </p>
+            <p> We must decide what we are going to add to the about page ... </p>
+        </div>
+    )
+}
+
+export default  PersonalGallery
