37f9d4a5267456c1c44ffa5b7b090349a649c0e6
Layla <layla.silbernberg@appliedtechnology.se>
2023-03-28 14:26:17 +0200

    Image pop up works!

--- virtual-museum-frontend/src/app/(Components)/Artworks.tsx
+++ virtual-museum-frontend/src/app/(Components)/Artworks.tsx
@@ -1,21 +1,24 @@
-import React, { ForwardRefExoticComponent, Ref, useRef, useState } from 'react'
+import React, { ForwardRefExoticComponent, Ref, useEffect, useRef, useState } from 'react'
 import { ArtworkProps } from '../(Props)/props'
 import Image from "next/image";
+import Popup from 'reactjs-popup';
+
+
 
 const Artworks = ({image_id, title, artist_display, place_of_origin, thumbnail}: ArtworkProps) => {
 
-    const [openImage, setOpenImage] = useState<Boolean>(false);
-    const imageRef = useRef<any>()
+    const [openImage, setOpenImage] = useState<boolean>(false);
+    const imageRef = useRef<HTMLDivElement>(null)
 
     openImage ? document.body.style.overflow = "hidden" : document.body.style.overflow = "auto";    
 
   return (
 
     <>
-    
-    {
-    (openImage &&  
-      <div className="largeImage__container">
+     <Popup open={openImage} closeOnDocumentClick onClose={() => setOpenImage(false)}>
+      <div 
+      id='largeImage__container'
+      className="largeImage__container">
       <Image
                   className='largeImage__artwork'
                   src={`https://www.artic.edu/iiif/2/${image_id}/full/843,/0/default.jpg`}
@@ -23,13 +26,11 @@ const Artworks = ({image_id, title, artist_display, place_of_origin, thumbnail}:
                   width={600}
                   height={600}
                   onClick={() => setOpenImage(false)}
-                  priority 
-                  ref={imageRef}
+                  priority
                   />
       </div>
-     )
-     } 
-
+    </Popup>
+    
       <div className="image__card">
               <Image
                   className='image__artwork'
@@ -37,8 +38,7 @@ const Artworks = ({image_id, title, artist_display, place_of_origin, thumbnail}:
                   alt={`${thumbnail.alt_text}`}
                   width={300}
                   height={300}
-                  onClick={() => {
-                    setOpenImage(true)}}
+                  onClick={() => setOpenImage(true)}
                   priority />
         </div></>
   )
