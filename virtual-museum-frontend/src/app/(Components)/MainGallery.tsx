bb52223d148fe5e37427b4ccf9459a8d50309fd4
Layla <layla.silbernberg@appliedtechnology.se>
2023-04-05 17:10:26 +0200

    chevron changes back

--- virtual-museum-frontend/src/app/(Components)/MainGallery.tsx
+++ virtual-museum-frontend/src/app/(Components)/MainGallery.tsx
@@ -26,23 +26,6 @@ position:'absolute'}}
     fontSize='inherit'/>
   </IconButton>
   </Box>
-  <Box
-  sx={{position: 'absolute',
-  flexDirection: 'row',
-  width: '100vw',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-start',
-  top: '50%'}}>
-  <IconButton
-  sx={{color: 'white',
-position:'absolute'}}
-  size='large'
-  onClick={() => router.push('/PC-14')}>
-    <ArrowBackIos
-    fontSize='inherit'/>
-  </IconButton>
-  </Box>
         <Grid container
           className='gallery__grid'
         >
